036e04740d85a28d5d3d57a18756664cb10dc7e1
Mike Lloyd <mike.lloyd03@pm.me>
2023-12-26 22:27:03 -0800

    Take user input

--- android/app/src/main/java/com/example/calc2/MainActivity.kt
+++ android/app/src/main/java/com/example/calc2/MainActivity.kt
@@ -3,19 +3,27 @@ package com.example.calc2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.calc2.ui.theme.Calc2Theme
 import com.example.calc2.shared.evaluate
+import com.example.calc2.ui.theme.Calc2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val result = evaluate("2*9").toString()
         super.onCreate(savedInstanceState)
         setContent {
             Calc2Theme {
@@ -24,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("2 * 9 = $result")
+                    InputField()
                 }
             }
         }
@@ -32,17 +40,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun InputField() {
+    var input by remember { mutableStateOf("") }
+    var answer by remember {
+        mutableDoubleStateOf(0.0)
+    }
+
+    Column {
+        Row {
+            TextField(
+                value = input,
+                onValueChange = { input = it },
+                singleLine = true,
+            )
+
+            Button(
+                onClick = {
+                    try {
+                        answer = evaluate(input)
+                    } catch (e: Exception) {
+                        println(e)
+                    }
+
+                },
+            ) {
+                Text("Evaluate")
+            }
+        }
+        Text(answer.toString())
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun InputFieldPreview() {
     Calc2Theme {
-        Greeting("Android")
+        InputField()
     }
 }
