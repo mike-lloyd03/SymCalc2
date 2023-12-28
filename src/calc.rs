59db5ba0022a5a9704156a7ee63c17402c9e6e1a
Mike Lloyd <mike.lloyd03@pm.me>
2023-12-27 23:03:46 -0800

    Add Calc type

---
+++ src/calc.rs
@@ -0,0 +1,119 @@
+use std::sync::{atomic::AtomicUsize, Arc, Mutex, RwLock};
+
+use crate::error::ArithmeticError;
+use kalk::parser;
+
+#[derive(uniffi::Object)]
+pub struct Calc {
+    history: RwLock<Vec<HistoryItem>>,
+    // history: RwLock<Vec<(String, f64)>>,
+}
+
+#[derive(uniffi::Object, Debug)]
+pub struct HistoryItem {
+    equation: RwLock<String>,
+    solution: RwLock<f64>,
+}
+
+impl HistoryItem {
+    pub fn new(equation: String, solution: f64) -> Self {
+        let equation = RwLock::new(equation);
+        let solution = RwLock::new(solution);
+
+        Self { equation, solution }
+    }
+}
+
+#[uniffi::export]
+impl Calc {
+    #[uniffi::constructor]
+    pub fn new() -> Arc<Self> {
+        let history = RwLock::new(vec![]);
+        Arc::new(Self { history })
+    }
+
+    pub fn evaluate(&self, expr: String) -> Result<f64, ArithmeticError> {
+        let mut parser_context = parser::Context::new();
+        let result = parser::eval(&mut parser_context, &expr)
+            .map_err(|_| ArithmeticError::Error)?
+            .ok_or(ArithmeticError::Error)?;
+
+        self.push_history(expr, result.to_f64());
+
+        Ok(result.to_f64())
+    }
+
+    fn push_history(&self, equation: String, solution: f64) {
+        let hist_item = HistoryItem::new(equation, solution);
+
+        if let Ok(mut hist_lock) = self.history.write() {
+            hist_lock.push(hist_item)
+        }
+    }
+
+    // pub fn get_history(&self) -> Vec<HistoryItem> {
+    //     if let Ok(history) = self.history.read() {
+    //         return history.into();
+    //     }
+    // }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_simple_arithmetic() -> Result<(), ArithmeticError> {
+//         let mut calc = Calc::new();
+
+//         assert_eq!(calc.evaluate("2 + 2".into())?, 4.0);
+//         assert_eq!(calc.evaluate("2 - 2".into())?, 0.0);
+//         assert_eq!(calc.evaluate("2 * 2".into())?, 4.0);
+//         assert_eq!(calc.evaluate("2 (2)".into())?, 4.0);
+//         assert_eq!(calc.evaluate("2 / 2".into())?, 1.0);
+//         Ok(())
+//     }
+
+//     #[test]
+//     fn test_errors() -> Result<(), ArithmeticError> {
+//         let mut calc = Calc::new();
+
+//         assert!(calc.evaluate("2 x 2".into()).is_err());
+//         Ok(())
+//     }
+
+//     #[test]
+//     fn test_complex_arithmetic() -> Result<(), ArithmeticError> {
+//         let mut calc = Calc::new();
+
+//         assert_eq!(calc.evaluate("2 (5 + 7)".into())?, 24.0);
+//         Ok(())
+//     }
+
+//     #[test]
+//     fn test_history() -> Result<(), ArithmeticError> {
+//         let mut calc = Calc::new();
+//         calc.evaluate("2 * 7".into())?;
+//         calc.evaluate("sqrt(81)".into())?;
+//         calc.evaluate("27 / 9 + 3".into())?;
+
+//         let expect = vec![
+//             HistoryItem {
+//                 equation: "2 * 7".into(),
+//                 solution: 14.0,
+//             },
+//             HistoryItem {
+//                 equation: "sqrt(81)".into(),
+//                 solution: 9.0,
+//             },
+//             HistoryItem {
+//                 equation: "27 / 9 + 3".into(),
+//                 solution: 6.0,
+//             },
+//         ];
+
+//         assert_eq!(calc.get_history(), expect);
+
+//         Ok(())
+//     }
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,4 @@
-use error::ArithmeticError;
-use kalk::parser;
-
+mod calc;
 mod error;
 
 uniffi::setup_scaffolding!("calc2");
-
-#[uniffi::export]
-pub fn evaluate(expr: String) -> Result<f64, ArithmeticError> {
-    let mut parser_context = parser::Context::new();
-    let result = parser::eval(&mut parser_context, &expr)
-        .map_err(|_| ArithmeticError::Error)?
-        .ok_or(ArithmeticError::Error)?;
-
-    Ok(result.to_f64())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_simple_arithmetic() -> Result<(), ArithmeticError> {
-        assert_eq!(evaluate("2 + 2".into())?, 4.0);
-        assert_eq!(evaluate("2 - 2".into())?, 0.0);
-        assert_eq!(evaluate("2 * 2".into())?, 4.0);
-        assert_eq!(evaluate("2 (2)".into())?, 4.0);
-        assert_eq!(evaluate("2 / 2".into())?, 1.0);
-        Ok(())
-    }
-
-    #[test]
-    fn test_errors() -> Result<(), ArithmeticError> {
-        assert!(evaluate("2 x 2".into()).is_err());
-        Ok(())
-    }
-
-    #[test]
-    fn test_complex_arithmetic() -> Result<(), ArithmeticError> {
-        assert_eq!(evaluate("2 (5 + 7)".into())?, 24.0);
-        Ok(())
-    }
-}
