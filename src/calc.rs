c110a5bbe239c3bd877081d1e779d81084d9ab73
Mike Lloyd <mike.lloyd03@pm.me>
2024-01-01 22:54:06 -0800

    Store db pool on calc obj

--- src/calc.rs
+++ src/calc.rs
@@ -3,11 +3,47 @@ use std::convert::Into;
 use crate::{db, error::CalcError};
 use async_std::task;
 use kalk::parser;
+use sqlx::SqlitePool;
 use uniffi::deps::{anyhow::Context, log::info};
 
 #[derive(uniffi::Object)]
 pub struct Calc {
-    db_path: String,
+    db: SqlitePool,
+}
+
+#[uniffi::export]
+impl Calc {
+    #[allow(clippy::new_without_default)]
+    #[uniffi::constructor]
+    pub fn new(data_path: &str) -> Result<Self, CalcError> {
+        info!("Creating Calc instance");
+        let db_path = format!("sqlite://{}/data.db", data_path.trim_end_matches('/'));
+        let db = task::block_on(db::connect(&db_path))?;
+        Ok(Self { db })
+    }
+
+    pub fn evaluate(&self, expr: String) -> Result<f64, CalcError> {
+        info!("Evaluating {expr}");
+        let mut parser_context = parser::Context::new();
+        let result = parser::eval(&mut parser_context, &expr)
+            .map_err(|_| CalcError::MathError)?
+            .ok_or(CalcError::MathError)?;
+
+        self.push_history(expr, result.to_f64())?;
+
+        Ok(result.to_f64())
+    }
+
+    fn push_history(&self, equation: String, solution: f64) -> Result<(), CalcError> {
+        info!("Pushing history item {equation} = {}", solution.to_string());
+        let hist_item = HistoryItem::new(equation, solution);
+        task::block_on(hist_item.create(&self.db))
+    }
+
+    pub fn get_history(&self) -> Result<Vec<HistoryItem>, CalcError> {
+        info!("Getting all history");
+        task::block_on(HistoryItem::get_all(&self.db))
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, uniffi::Record)]
@@ -26,61 +62,22 @@ impl HistoryItem {
         }
     }
 
-    async fn create(&self, db_path: &str) -> Result<(), CalcError> {
-        let db = db::connect(db_path).await?;
-
+    async fn create(&self, pool: &SqlitePool) -> Result<(), CalcError> {
         Ok(sqlx::query!(
             "insert into history (equation, solution) values (?, ?)",
             self.equation,
             self.solution
         )
-        .execute(&db)
+        .execute(pool)
         .await
         .map(|_| ())?)
     }
 
-    async fn get_all(db_path: &str) -> Result<Vec<Self>, CalcError> {
-        let db = db::connect(db_path).await?;
-
+    async fn get_all(pool: &SqlitePool) -> Result<Vec<Self>, CalcError> {
         Ok(sqlx::query_as!(Self, "select * from history")
-            .fetch_all(&db)
+            .fetch_all(pool)
             .await
             .context("Failed to get all history.")?)
-    }
-}
-
-#[uniffi::export]
-impl Calc {
-    #[allow(clippy::new_without_default)]
-    #[uniffi::constructor]
-    pub fn new(data_path: &str) -> Self {
-        info!("Creating Calc instance");
-        let db_path = format!("sqlite://{}/data.db", data_path.trim_end_matches('/'));
-        println!("{db_path}");
-        Self { db_path }
-    }
-
-    pub fn evaluate(&self, expr: String) -> Result<f64, CalcError> {
-        info!("Evaluating {expr}");
-        let mut parser_context = parser::Context::new();
-        let result = parser::eval(&mut parser_context, &expr)
-            .map_err(|_| CalcError::MathError)?
-            .ok_or(CalcError::MathError)?;
-
-        self.push_history(expr, result.to_f64())?;
-
-        Ok(result.to_f64())
-    }
-
-    fn push_history(&self, equation: String, solution: f64) -> Result<(), CalcError> {
-        info!("Pushing history item {equation} = {}", solution.to_string());
-        let hist_item = HistoryItem::new(equation, solution);
-        task::block_on(hist_item.create(&self.db_path))
-    }
-
-    pub fn get_history(&self) -> Result<Vec<HistoryItem>, CalcError> {
-        info!("Getting all history");
-        task::block_on(HistoryItem::get_all(&self.db_path))
     }
 }
 
@@ -92,7 +89,7 @@ mod tests {
 
     #[sqlx::test]
     fn test_simple_arithmetic() -> Result<(), CalcError> {
-        let calc = Calc::new(DATA_PATH);
+        let calc = Calc::new(DATA_PATH)?;
 
         assert_eq!(calc.evaluate("2 + 2".into())?, 4.0);
         assert_eq!(calc.evaluate("2 - 2".into())?, 0.0);
@@ -104,7 +101,7 @@ mod tests {
 
     #[sqlx::test]
     fn test_errors() -> Result<(), CalcError> {
-        let calc = Calc::new(DATA_PATH);
+        let calc = Calc::new(DATA_PATH)?;
 
         assert!(calc.evaluate("2 x 2".into()).is_err());
         Ok(())
@@ -112,7 +109,7 @@ mod tests {
 
     #[sqlx::test]
     fn test_complex_arithmetic() -> Result<(), CalcError> {
-        let calc = Calc::new(DATA_PATH);
+        let calc = Calc::new(DATA_PATH)?;
 
         assert_eq!(calc.evaluate("2 (5 + 7)".into())?, 24.0);
         Ok(())
@@ -120,10 +117,11 @@ mod tests {
 
     #[sqlx::test]
     fn test_history() -> Result<(), CalcError> {
-        let calc = Calc::new(DATA_PATH);
+        let calc = Calc::new(DATA_PATH)?;
 
-        let db = db::connect(&calc.db_path).await?;
-        sqlx::query!("delete from history").execute(&db).await?;
+        sqlx::query!("delete from history")
+            .execute(&calc.db)
+            .await?;
 
         calc.evaluate("2 * 7".into())?;
         calc.evaluate("sqrt(81)".into())?;
